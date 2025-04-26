9a5ed48f17cfecbb69cc88abe3a150f7e4a84905
chahe-dridi <chaher.dridi.100@gmail.com>
2025-04-26 14:55:59 +0100

    update

--- workshop/apollo-server/resolvers.js
+++ workshop/apollo-server/resolvers.js
@@ -1,8 +1,26 @@
 // resolvers.js
 module.exports = {
+  Book: {
+    id: (parent) => parent._id.toString(),
+  },
   Query: {
+    book: async (_, { id }, { dataSources }) => {
+      return dataSources.bookAPI.getBookById(id);
+    },
     allBooks: async (_, __, { dataSources }) => {
       return dataSources.bookAPI.getAllBooks();
     },
-  }
+  },
+  Mutation: {
+    createBook: async (_, { input }, { dataSources }) => {
+      return dataSources.bookAPI.createBook(input);
+    },
+    updateBook: async (_, { id, input }, { dataSources }) => {
+      return dataSources.bookAPI.updateBook(id, input);
+    },
+    deleteBook: async (_, { id }, { dataSources }) => {
+      await dataSources.bookAPI.deleteBook(id);
+      return "Book with id"+id+" deleted successfully.";
+    },
+  },
 };
